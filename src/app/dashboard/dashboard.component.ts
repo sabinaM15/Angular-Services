f5d3403272bcc1f354b480c9263b9db634511029
Sabina-Maria Pintilie <sabina-mariapintilie@Sabina-Marias-MacBook-Air.local>
2022-10-19 16:11:28 +0300

    19 oct

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Book } from "app/models/book";
+import { allBooks, allReaders } from 'app/data';
+import { Reader } from "app/models/reader";
+import { LoggerService } from 'app/core/logger.service';
+import { DataService } from 'app/core/data.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styles: []
+})
+export class DashboardComponent implements OnInit {
+
+  allBooks: Book[] = allBooks;
+  allReaders: Reader[] = allReaders;
+  mostPopularBook: Book = allBooks[0];
+
+  constructor(private loggerService: LoggerService,
+              private dataService: DataService) {
+   }
+
+  ngOnInit() {
+    this.allBooks = this.dataService.getAllBooks();
+    this.allReaders = this.dataService.getAllReaders();
+    this.mostPopularBook = this.dataService.mostPopularBook;
+  }
+
+  deleteBook(bookID: number): void {
+    console.warn(`Delete book not yet implemented (bookID: ${bookID}).`);
+  }
+
+  deleteReader(readerID: number): void {
+    console.warn(`Delete reader not yet implemented (readerID: ${readerID}).`);
+  }
+
+}
